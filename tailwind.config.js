26620733ce865bd13a129cfbdf96898d90786cc5
Alexandre Schwanke da Costa <alexandreschwc@gmail.com>
2022-08-02 18:36:38 -0300

    estilos parte 1, mobile quase finalizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./*"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "serif": ['Manrope', "sans-serif"]
+    },
+    colors: {
+      "cyan": {
+        300: "#5DEADC",
+        500: "#10D5C2"
+      },
+      "red": {
+        100: "#FEECE7",
+        500: "#FF8C66"
+      },
+      "indigo": {
+        200: "#CDD7EE",
+        300: "#BDCCFF"
+      },
+      "blue": {
+        50: "#FAFBFF",
+        100: "#EAEEFB"
+      },
+      "slate": {
+        400: "#858FAD",
+        800: "#293356"
+      },
+      "white" : "#ffffff"
+    },
+    extend: {
+      backgroundImage: {
+        "background-cover" : "url('../images/bg-pattern.svg')",
+        "circles" : "url(../images/pattern-circles.svg)"
+      }
+    },
   },
   plugins: [],
 }
